e9f7771fed0a3036086d561ac4458b4f20a89151
Thomas J. Higgins <s3529120@studennt.rmit.edu.au>
2018-03-24 20:38:34 +1100

    added base env database config and shares migration

---
+++ database/migrations/2018_03_24_091416_create_shares_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('code');
+			$table->decimal('value',8,4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}

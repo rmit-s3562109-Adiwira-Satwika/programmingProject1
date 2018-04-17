d1347955aac22f1f300596c4690ae96fd8706b23
rmit-s3529120-thomas-higgins <s3529120@student.rmit.edu.au>
2018-04-18 08:31:09 +1000

    Expanded historical request capabilities

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -23,19 +23,33 @@ class ShareController extends Controller
 
 	/** Request share data over time based on input
 	*	@param $code asx stock code
-	*	@param $time Time period for each value 1,5,15,30 or 60 only
+	*	@param $timeSeries Time series to result results over, INTRADAY, DAILY, WEEKLY, MONTHLY only
+	*	@param $time Time period for each value 1,5,15,30 or 60 only, or null if not intraday
 	*	@param $limit Number of responses to limit return to
 	*	@return Returns false on invalid input otherwise returns array of values
 	*/
-	public function requestShareData($code,$time,$limit){
-		//Check for valid time and limit input
-		if($time!=1||$time!=5||$time!=15||$time!=30||$time!=60||$limit>100){
+	public function requestShareData($code,$timeSeries,$time,$limit){
+
+		//Check for valid time series
+		if($timeSeries!="INTRADAY"||$timeSeries!="DAILY"||$timeSeries!="WEEKLY"||$timeSeries!="MONTHLY"){
 			return false;
 		}
 
+		if($timeSeries=="INTRADAY"){
+			$timestr="&interval=+"$time"+min"
+		
+			//Check for valid time and limit input
+			if($time!=1||$time!=5||$time!=15||$time!=30||$time!=60||$limit>100){
+				return false;
+			}
+		}else{
+			$timestr="";
+		}
+
 		//Get api response and grab data section
-		$response=json_decode(file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+$code+".ax&interval=+"$time"+min&apikey=6DD89FIYMJ57CPGO"));
-        $section= $response["Time Series ("+$time+"min)"];
+		$response=json_decode(file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_"+
+			$timeSeries+"&symbol="+$code+".ax"+$timestr+"&apikey=6DD89FIYMJ57CPGO"));
+        $section= $response[1];
 
         //Add each value to limit
         $array=array();

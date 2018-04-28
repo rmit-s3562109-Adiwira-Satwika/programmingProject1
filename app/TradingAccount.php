0b38fe13b23c589f92debfca524163b406e0ee8b
Thomas J. Higgins <s3529120@studennt.rmit.edu.au>
2018-04-28 17:42:31 +1000

    Added basic friends functionality

--- app/TradingAccount.php
+++ app/TradingAccount.php
@@ -3,10 +3,33 @@
 namespace ShareMarketGame;
 
 use Illuminate\Database\Eloquent\Model;
+use ShareMarketGame\Friend;
 
 class TradingAccount extends Model
 {
     protected $primaryKey = 'nickname';
     public $incrementing = false;
     protected $fillable = ['nickname','balance','user_id'];
+
+    public function isFriend($friendName){
+
+    	//Gte all friends
+    	$friendds=Friend::where('nickname', '=', $this->nickname, 'or')->where('friend', '=', $this->nickname);
+
+    	//Check if no friends exist
+    	if($friends->isEmpty()){
+    		return false;
+    	}
+
+    	//CHeck if each friend matches given name
+    	foreach ($friends as $friend) {
+    		//Return true if match found
+    		if($friend->nickname==$friendname || $friend->friend==$friendname){
+    			return true;
+    		}
+    	}
+
+    	//Return false if friend not found
+    	return false;
+    }
 }
---
+++ database/migrations/2018_04_28_071905_create_friends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->string('nickname');
+            $table->string('friend');
+
+            $table->foreign('nickname')->references('nickname')->on('trading_accounts');
+            $table->foreign('friend')->references('nickname')->on('trading_accounts');
+            
+            $table->primary('nickname','friend');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}

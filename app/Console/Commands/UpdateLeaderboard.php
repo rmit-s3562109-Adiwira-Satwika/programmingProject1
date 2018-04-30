fd353359ffd75be4fee3d509b67afc829f0df589
rmit-s3529120-thomas-higgins <s3529120@student.rmit.edu.au>
2018-04-30 13:30:22 +1000

    Syntax fix

--- app/Console/Commands/UpdateLeaderboard.php
+++ app/Console/Commands/UpdateLeaderboard.php
@@ -66,7 +66,7 @@ class UpdateLeaderboard extends Command
         $total=$total+$account->balance;
 
         //Retrieve all holdings
-        $holdings=Holding::find('nickname', $acc->nickname);
+        $holdings=Holding::find('nickname', $account->nickname);
 
         //Add value of each holding to total
         foreach($holdings as $hold){
